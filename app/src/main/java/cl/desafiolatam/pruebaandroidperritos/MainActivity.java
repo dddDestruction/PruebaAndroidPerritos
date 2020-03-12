90b6e78294bffd2519a2055baab29f3f63781f35
Nicolas <nifradich@hotmail.com>
2020-03-12 12:06:13 -0300

    inicio

---
+++ app/src/main/java/cl/desafiolatam/pruebaandroidperritos/MainActivity.java
@@ -0,0 +1,14 @@
+package cl.desafiolatam.pruebaandroidperritos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
